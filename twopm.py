aedba0983a39545f8687582bebe783d14cf42bc3
Yen-00 <2561586@edu.vaniercollege.qc.ca>
2026-01-19 15:06:13 -0500

    Add files via upload

---
+++ twopm.py
@@ -0,0 +1,19 @@
+def hello():
+    print("Hello world")
+
+hello()
+
+def quiz(): 
+    name=input("enter your name ")
+    age=int(input("enter your age: "))
+    height=float(input("enter your height: (in meters)"))
+
+    print(f"hello {name}")
+    print(f"you are {age} years old")
+    print(f"your height is {height} meters")
+
+quiz()
+
+
+
+
